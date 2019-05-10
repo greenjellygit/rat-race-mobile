fe327d027b3a3db6b22a9c2abc840cb7fb8af1d2
Jacek Gołębiewski <jacekg1993@gmail.com>
2019-05-10 22:41:42 +0200

    roomDetails example

--- rat-race-mobile/src/app/room/room.component.ts
+++ rat-race-mobile/src/app/room/room.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.scss']
 })
 export class RoomComponent implements OnInit {
+  roomDetails: Observable<any> = this.socket.fromEvent<string[]>('roomDetails');
 
-  constructor() { }
+  constructor(private socket: Socket) { }
 
   ngOnInit() {
+    this.roomDetails.subscribe((data) => {
+      console.log(`Details ${JSON.stringify(data)}`);
+    });
   }
 
 }
